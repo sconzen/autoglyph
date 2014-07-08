57274cdc01cd51f6245ee86d41c6686a0425b671
Stephan <sconzen@gmail.com>
2014-07-07 21:43:47 -0400

    Initial Commit
    Siege of Orgrimmar glyphs file.
    Does not currently contain any glyphs.

---
+++ soo.lua
@@ -0,0 +1,24 @@
+-- Siege of Ogrimmar
+-- use array code format:
+	-- class/spec code ie. cc = Survival Hunter
+	-- tier code ie. soo = Siege of Ogrimmar
+	-- boss code ie. a = Immerseus 
+	-- Example 1 - Survival Hunter - Siege of Ogrimmar - Immerseus - ccsooa
+	-- Example 2 - Elemental Shaman - Siege of Ogrimmar - Sha of Pride - iasood
+-- 1
+-- 2
+-- 3
+-- 4
+
+-- 5
+-- 6
+-- 7
+-- 8
+
+-- 9
+-- 10
+-- 11
+
+-- 12
+-- 13
+-- 14
