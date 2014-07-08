09ca6627323a85a5bfee7d51fc73ef4e3696a529
Stephan <sconzen@gmail.com>
2014-07-07 21:42:51 -0400

    Initial Commit
    Contains all class glyphs.
    Contains general "cookie cutter" glyphs for all classes.

---
+++ glyphs.lua
@@ -0,0 +1,549 @@
+blank = '|cffff0000'..'Empty Glyph'..'|r'
+nyi = '|cffff0000'..'Not Yet Implemented'..'|r'
+
+-- Death Knight
+	aallglyphs = {
+		"Glyph of Anti-Magic Shell",
+		"Glyph of Dancing Rune Weapon",
+		"Glyph of Dark Succor",
+		"Glyph of Death and Decay",
+		"Glyph of Death Coil",
+		"Glyph of Death Grip",
+		"Glyph of Enduring Infection",
+		"Glyph of Festering Blood",
+		"Glyph of Icebound Fortitude",
+		"Glyph of Icy Touch",
+		"Glyph of Mind Freeze",
+		"Glyph of Outbreak",
+		"Glyph of Pestilence",
+		"Glyph of Pillar of Frost",
+		"Glyph of Regenerative Magic",
+		"Glyph of Shifting Presences",
+		"Glyph of Strangulate",
+		"Glyph of Swift Death",
+		"Glyph of Unholy Command",
+		"Glyph of the Loud Horn",
+		"Glyph of Unholy Frenzy",
+		"Glyph of Vampiric Blood",
+		"Glyph of Army of the Dead",
+		"Glyph of Corpse Explosion",
+		"Glyph of Death's Embrace",
+		"Glyph of Death Gate",
+		"Glyph of Foul Menagerie",
+		"Glyph of the Geist",
+		"Glyph of Horn of Winter",
+		"Glyph of the Long Winter",
+		"Glyph of Path of Frost",
+		"Glyph of Resilient Grip",
+		"Glyph of the Skeleton",
+		"Glyph of Tranquil Grip" };
+
+	-- Blood
+		aa = {aallglyphs[22],aallglyphs[1],aallglyphs[4],aallglyphs[5],aallglyphs[8],aallglyphs[9],aallglyphs[10],aallglyphs[13],aallglyphs[15],aallglyphs[18],aallglyphs[31],aallglyphs[30]}
+	-- Frost
+		ab = {aallglyphs[13],aallglyphs[1],aallglyphs[5],aallglyphs[16],aallglyphs[15],aallglyphs[18],aallglyphs[31],aallglyphs[23],aallglyphs[30],aallglyphs[34]}
+	-- Unholy
+		ac = {aallglyphs[13],aallglyphs[1],aallglyphs[5],aallglyphs[8],aallglyphs[15],aallglyphs[16],aallglyphs[18],aallglyphs[21],aallglyphs[31],aallglyphs[23],aallglyphs[30],aallglyphs[25],aallglyphs[34]}
+
+-- Druid
+	ballglyphs = {
+		"Glyph of Barkskin",
+		"Glyph of Blooming",
+		"Glyph of Cat Form",
+		"Glyph of Cyclone",
+		"Glyph of Dash",
+		"Glyph of Efflorescence",
+		"Glyph of Entangling Roots",
+		"Glyph of Fae Silence",
+		"Glyph of Faerie Fire",
+		"Glyph of Ferocious Bite",
+		"Glyph of Frenzied Regeneration",
+		"Glyph of Guided Stars",
+		"Glyph of Healing Touch",
+		"Glyph of Hurricane",
+		"Glyph of Innervate",
+		"Glyph of Master Shapeshifter",
+		"Glyph of Maul",
+		"Glyph of Might of Ursoc",
+		"Glyph of Nature's Grasp",
+		"Glyph of Omens",
+		"Glyph of Pounce",
+		"Glyph of Prowl",
+		"Glyph of Rebirth",
+		"Glyph of Regrowth",
+		"Glyph of Rejuvenation",
+		"Glyph of Savagery",
+		"Glyph of Skull Bash",
+		"Glyph of Shred",
+		"Glyph of Stampede",
+		"Glyph of Stampeding Roar",
+		"Glyph of Survival Instincts",
+		"Glyph of Wild Growth",
+		"Glyph of Aquatic Form",
+		"Glyph of Charm Woodland Creature",
+		"Glyph of Grace",
+		"Glyph of One With Nature",
+		"Glyph of Stars",
+		"Glyph of the Chameleon",
+		"Glyph of the Cheetah",
+		"Glyph of the Orca",
+		"Glyph of the Predator",
+		"Glyph of the Sprouting Mushroom",
+		"Glyph of the Stag",
+		"Glyph of the Treant"};
+
+	-- Druid - Balance
+		ba = {ballglyphs[12],ballglyphs[30],ballglyphs[29],ballglyphs[23],ballglyphs[35]}
+	-- Druid - Guardian
+		bb = {ballglyphs[17],ballglyphs[8],ballglyphs[11],ballglyphs[18],ballglyphs[23],ballglyphs[30],ballglyphs[31],ballglyphs[35]}
+	-- Druid - Feral
+		bc = {ballglyphs[26],ballglyphs[3],ballglyphs[10],ballglyphs[23],ballglyphs[28],ballglyphs[30]}
+	-- Druid - Restoration
+		bd = {ballglyphs[6],ballglyphs[13],ballglyphs[32],ballglyphs[15],ballglyphs[23],ballglyphs[24],ballglyphs[25],ballglyphs[35],ballglyphs[42]}
+
+-- Hunter
+	callglyphs = {
+		"Glyph of Aimed Shot",
+		"Glyph of Animal Bond",
+		"Glyph of Black Ice",
+		"Glyph of Camouflage",
+		"Glyph of Chimera Shot",
+		"Glyph of Deterrence",
+		"Glyph of Disengage",
+		"Glyph of Distracting Shot",
+		"Glyph of Endless Wrath",
+		"Glyph of Enduring Deceit",
+		"Glyph of Explosive Trap",
+		"Glyph of Freezing Trap",
+		"Glyph of Ice Trap",
+		"Glyph of Icy Solace",
+		"Glyph of the Lean Pack",
+		"Glyph of Liberation",
+		"Glyph of Master's Call",
+		"Glyph of Mend Pet",
+		"Glyph of Mending",
+		"Glyph of Mirrored Blades",
+		"Glyph of Misdirection",
+		"Glyph of No Escape",
+		"Glyph of Pathfinding",
+		"Glyph of Scatter Shot",
+		"Glyph of Scattering",
+		"Glyph of Snake Trap",
+		"Glyph of Tranquilizing Shot",
+		"Glyph of Aspect of the Beast",
+		"Glyph of Aspect of the Cheetah",
+		"Glyph of Aspect of the Pack",
+		"Glyph of Aspects",
+		"Glyph of Direction",
+		"Glyph of Fetch",
+		"Glyph of Fireworks",
+		"Glyph of Lesser Proportion",
+		"Glyph of Marking",
+		"Glyph of Revive Pet",
+		"Glyph of Stampede",
+		"Glyph of Tame Beast"};
+	-- Hunter - Beast Mastery
+		ca = {callglyphs[2],callglyphs[6],callglyphs[16],callglyphs[29],callglyphs[30],callglyphs[37]}
+	-- Hunter - Marksmanship
+		cb = {callglyphs[1],callglyphs[2],callglyphs[5],callglyphs[29],callglyphs[30],callglyphs[37]}
+	-- Hunter - Survival
+		cc = {callglyphs[2],callglyphs[6],callglyphs[16],callglyphs[29],callglyphs[30],callglyphs[37]}
+
+-- Mage
+	dallglyphs = {
+		"Glyph of Arcane Explosion",
+		"Glyph of Arcane Power",
+		"Glyph of Armors",
+		"Glyph of Blink",
+		"Glyph of Combustion",
+		"Glyph of Cone of Cold",	
+		"Glyph of Counterspell",
+		"Glyph of Deep Freeze",
+		"Glyph of Evocation",
+		"Glyph of Frost Nova",
+		"Glyph of Frostfire Bolt",
+		"Glyph of Ice Block",
+		"Glyph of Icy Veins",
+		"Glyph of Inferno Blast",
+		"Glyph of Mana Gem",
+		"Glyph of Polymorph",
+		"Glyph of Rapid Displacement",
+		"Glyph of Remove Curse",
+		"Glyph of Slow",
+		"Glyph of Spellsteal",
+		"Glyph of Splitting Ice",
+		"Glyph of Water Elemental",
+		"Glyph of Arcane Language",
+		"Glyph of the Bear Cub",
+		"Glyph of Condensation",
+		"Glyph of Conjure Familiar",
+		"Glyph of Crittermorph",
+		"Glyph of Evaporation",
+		"Glyph of Illusion",
+		"Glyph of Loose Mana",
+		"Glyph of Mirror Image",
+		"Glyph of Momentum",
+		"Glyph of the Monkey",
+		"Glyph of the Penguin",
+		"Glyph of the Porcupine",
+		"Glyph of Rapid Teleportation",
+		"Glyph of the Unbound Elemental"};
+	-- Mage - Arcane
+		da = {dallglyphs[15],dallglyphs[2],dallglyphs[9],dallglyphs[1],dallglyphs[7],dallglyphs[31],dallglyphs[30],dallglyphs[32]}
+	-- Mage - Fire
+		db = {dallglyphs[5],dallglyphs[9],dallglyphs[14],dallglyphs[17],dallglyphs[31],dallglyphs[32]}
+	-- Mage - Frost
+		dc = {dallglyphs[21],dallglyphs[13],dallglyphs[22],dallglyphs[1],dallglyphs[7],dallglyphs[9],dallglyphs
+		[32]}
+
+-- Monk
+	eallglyphs = {
+		"Glyph of Afterlife",
+		"Glyph of Breath of Fire",
+		"Glyph of Clash",
+		"Glyph of Detox",
+		"Glyph of Enduring Healing Sphere",
+		"Glyph of Fists of Fury",
+		"Glyph of Fortifying Brew",
+		"Glyph of Fortuitous Spheres",
+		"Glyph of Guard",
+		"Glyph of Leer of the Ox",
+		"Glyph of Life Cocoon",
+		"Glyph of Mana Tea",
+		"Glyph of Nimble Brew",
+		"Glyph of Paralysis",
+		"Glyph of Rapid Rolling",
+		"Glyph of Renewing Mists",
+		"Glyph of Sparring",
+		"Glyph of Spinning Crane Kick",
+		"Glyph of Surging Mist",
+		"Glyph of Targeted Expulsion",
+		"Glyph of Touch of Death",
+		"Glyph of Touch of Karma",
+		"Glyph of Transcendence",
+		"Glyph of Zen Meditation",
+		"Glyph of Blackout Kick",
+		"Glyph of Crackling Tiger Lightning",
+		"Glyph of Fighting Pose",
+		"Glyph of Flying Serpent Kick",
+		"Glyph of Honor",
+		"Glyph of Jab",
+		"Glyph of Rising Tiger Kick",
+		"Glyph of Spinning Fire Blossom",
+		"Glyph of Spirit Roll",
+		"Glyph of Water Roll",
+		"Glyph of Zen Flight"};
+	-- Monk - Brewmaster
+		ea = {eallglyphs[7],eallglyphs[8],eallglyphs[18],eallglyphs[2],eallglyphs[3],eallglyphs[4],eallglyphs[9],eallglyphs[10],eallglyphs[15]}
+	-- Monk - Windwalker
+		eb = {eallglyphs[18],eallglyphs[22],eallglyphs[4],eallglyphs[8],eallglyphs[21],eallglyphs[23],eallglyphs[25],eallglyphs[28]}
+	-- Monk - Mistweaver
+		ec = {eallglyphs[12],eallglyphs[16],eallglyphs[4],eallglyphs[8],eallglyphs[18],eallglyphs[20],eallglyphs[24]}
+
+-- Paladin
+	fallglyphs = {
+		"Glyph of the Alabaster Shield",
+		"Glyph of Avenging Wrath",
+		"Glyph of the Battle Healer",
+		"Glyph of Blinding Light",
+		"Glyph of Beacon of Light",
+		"Glyph of Blessed Life",
+		"Glyph of Burden of Guilt",
+		"Glyph of Consecration",
+		"Glyph of Dazing Shield",
+		"Glyph of Denounce",
+		"Glyph of Divine Plea",
+		"Glyph of Divine Protection",
+		"Glyph of Divine Shield",
+		"Glyph of Divine Storm",
+		"Glyph of Divinity",
+		"Glyph of Double Jeopardy",
+		"Glyph of Final Wrath",
+		"Glyph of Flash of Light",
+		"Glyph of Focused Shield",
+		"Glyph of Hammer of the Righteous",
+		"Glyph of Hand of Sacrifice",
+		"Glyph of Harsh Words",
+		"Glyph of Holy Shock",
+		"Glyph of Holy Wrath",
+		"Glyph of Illumination",
+		"Glyph of Immediate Truth",
+		"Glyph of Inquisition",
+		"Glyph of Light of Dawn",
+		"Glyph of Mass Exorcism",
+		"Glyph of Protector of the Innocent",
+		"Glyph of Templar's Verdict",
+		"Glyph of Turn Evil",
+		"Glyph of Word of Glory",
+		"Glyph of Bladed Judgment",
+		"Glyph of Contemplation",
+		"Glyph of the Falling Avenger",
+		"Glyph of Fire From the Heavens",
+		"Glyph of Focused Wrath",
+		"Glyph of the Luminous Charger",
+		"Glyph of Pillar of Light",
+		"Glyph of the Righteous Retreat",
+		"Glyph of Seal of Blood",
+		"Glyph of the Exorcist",
+		"Glyph of the Mounted King",
+		"Glyph of Winged Vengeance"};
+
+	-- Holy
+		fa = {fallglyphs[11],fallglyphs[15],fallglyphs[2],fallglyphs[3],fallglyphs[12],fallglyphs[13],fallglyphs[18],fallglyphs[21],fallglyphs[25],fallglyphs[28],fallglyphs[41],fallglyphs[36]}
+	-- Protection
+		fb = {fallglyphs[1],fallglyphs[12],fallglyphs[3],fallglyphs[8],fallglyphs[13],fallglyphs[17],fallglyphs[19],fallglyphs[22],fallglyphs[41],fallglyphs[36]} 
+	-- Retribution	
+		fc = {fallglyphs[16],fallglyphs[29],fallglyphs[31],fallglyphs[14],fallglyphs[22],fallglyphs[41],fallglyphs[36]} 
+
+-- Priest
+	gallglyphs = {
+		"Glyph of Binding Heal",
+		"Glyph of Circle of Healing",
+		"Glyph of Deep Wells",
+		"Glyph of Dispel Magic",
+		"Glyph of Dispersion",
+		"Glyph of Fade",
+		"Glyph of Fear Ward",
+		"Glyph of Focused Mending",
+		"Glyph of Holy Fire",
+		"Glyph of Holy Nova",
+		"Glyph of Inner Fire",
+		"Glyph of Inner Sanctum",
+		"Glyph of Leap of Faith",
+		"Glyph of Levitate",
+		"Glyph of Lightwell",
+		"Glyph of Mass Dispel",
+		"Glyph of Mind Blast",
+		"Glyph of Mind Flay",
+		"Glyph of Mind Spike",
+		"Glyph of Penance",
+		"Glyph of Power Word: Shield",
+		"Glyph of Prayer of Mending",
+		"Glyph of Psychic Horror",
+		"Glyph of Psychic Scream",
+		"Glyph of Purify",
+		"Glyph of Reflective Shield",
+		"Glyph of Renew",
+		"Glyph of Scourge Imprisonment",
+		"Glyph of Shadow Word: Death",
+		"Glyph of Smite",
+		"Glyph of Spirit of Redemption",
+		"Glyph of Vampiric Embrace",
+		"Glyph of Weakened Soul",
+		"Glyph of Angels",
+		"Glyph of Borrowed Time",
+		"Glyph of Confession",
+		"Glyph of Dark Archangel",
+		"Glyph of the Heavens",
+		"Glyph of Holy Resurrection",
+		"Glyph of Inspired Hymns",
+		"Glyph of Shackle Undead",
+		"Glyph of Shadow",
+		"Glyph of Shifted Appearances",
+		"Glyph of Shadow Ravens",
+		"Glyph of Shadowy Friends",
+		"Glyph of the Sha",
+		"Glyph of the Val'kyr"};
+
+	-- Priest - Discipline	
+		ga = {gallglyphs[9],gallglyphs[20],gallglyphs[12],gallglyphs[6],gallglyphs[21],gallglyphs[25],gallglyphs[27],gallglyphs[30],gallglyphs[33]}
+	-- Priest - Shadow
+		gb = {gallglyphs[12],gallglyphs[18],gallglyphs[6],gallglyphs[14],gallglyphs[32],gallglyphs[33]}
+	-- Priest - Holy
+		gc = {gallglyphs[3],gallglyphs[12],gallglyphs[27],gallglyphs[2],gallglyphs[6],gallglyphs[15],gallglyphs[25],gallglyphs[22],gallglyphs[33]}
+
+-- Rogue
+	hallglyphs = {
+		"Glyph of Ambush",
+		"Glyph of Blade Flurry",
+		"Glyph of Blind",
+		"Glyph of Cheap Shot",
+		"Glyph of Cloak of Shadows",
+		"Glyph of Deadly Momentum",
+		"Glyph of Evasion",
+		"Glyph of Expose Armor",
+		"Glyph of Feint",
+		"Glyph of Garrote",
+		"Glyph of Gouge",
+		"Glyph of Hemorraghing Veins",
+		"Glyph of Kick",
+		"Glyph of Recovery",
+		"Glyph of Recuperate",
+		"Glyph of Redirect",
+		"Glyph of Shadow Walk",
+		"Glyph of Sharpened Knives",
+		"Glyph of Shiv",
+		"Glyph of Smoke Bomb",
+		"Glyph of Sprint",
+		"Glyph of Stealth",
+		"Glyph of Vanish",
+		"Glyph of Vendetta",
+		"Glyph of Blurred Speed",
+		"Glyph of Decoy",
+		"Glyph of Detection",
+		"Glyph of Disguise",
+		"Glyph of Distract",
+		"Glyph of Headhunting",
+		"Glyph of Hemorrhage",
+		"Glyph of Improved Distraction",
+		"Glyph of Killing Spree",
+		"Glyph of Pick Lock",
+		"Glyph of Pick Pocket",
+		"Glyph of Poisons",
+		"Glyph of Safe Fall",
+		"Glyph of Tricks of the Trade"};
+
+		ha = {hallglyphs[24],hallglyphs[9],hallglyphs[16],hallglyphs[5],hallglyphs[7],hallglyphs[8],hallglyphs[18],hallglyphs[19],hallglyphs[21],hallglyphs[38],hallglyphs[37],hallglyphs[36]}
+		hb = {hallglyphs[9],hallglyphs[16],hallglyphs[5],hallglyphs[7],hallglyphs[8],hallglyphs[18],hallglyphs[19],hallglyphs[21],hallglyphs[38],hallglyphs[37],hallglyphs[36]}
+		hc = {hallglyphs[1],hallglyphs[9],hallglyphs[16],hallglyphs[5],hallglyphs[7],hallglyphs[8],hallglyphs[12],hallglyphs[18],hallglyphs[19],hallglyphs[21],hallglyphs[38],hallglyphs[37],hallglyphs[36]}
+
+-- Shaman
+	iallglyphs = {
+		"Glyph of Capacitor Totem",
+		"Glyph of Chain Lightning",
+		"Glyph of Chaining",
+		"Glyph of Cleansing Waters",
+		"Glyph of Eternal Earth",
+		"Glyph of Feral Spirit",
+		"Glyph of Fire Elemental Totem",
+		"Glyph of Fire Nova",
+		"Glyph of Flame Shock",
+		"Glyph of Frost Shock",
+		"Glyph of Ghost Wolf",
+		"Glyph of Grounding Totem",
+		"Glyph of Healing Storm",
+		"Glyph of Healing Stream Totem",
+		"Glyph of Healing Wave",
+		"Glyph of Hex",
+		"Glyph of Lightning Shield",
+		"Glyph of Purge",
+		"Glyph of Purging",
+		"Glyph of Riptide",
+		"Glyph of Shamanistic Rage",
+		"Glyph of Spirit Walk",
+		"Glyph of Spiritwalker's Grace",
+		"Glyph of Telluric Currents",
+		"Glyph of Thunder",
+		"Glyph of Totemic Recall",
+		"Glyph of Totemic Vigor",
+		"Glyph of Unstable Earth",
+		"Glyph of Water Shield",
+		"Glyph of Wind Shear",
+		"Glyph of Astral Fixation",
+		"Glyph of Astral Recall",
+		"Glyph of the Compy",
+		"Glyph of Deluge",
+		"Glyph of Elemental Familiars",
+		"Glyph of Far Sight",
+		"Glyph of Lakestrider",
+		"Glyph of Lava Lash",
+		"Glyph of Lingering Ancestors",
+		"Glyph of Rain of Frogs",
+		"Glyph of the Spectral Wolf",
+		"Glyph of Spirit Raptors",
+		"Glyph of Spirit Wolf",
+		"Glyph of Thunderstorm",
+		"Glyph of Totemic Encirclement"};
+
+	-- Shaman - Elemental	
+		ia = {iallglyphs[2],iallglyphs[17],iallglyphs[7],iallglyphs[9],iallglyphs[23],iallglyphs[44]}
+	-- Shaman - Enhancement
+		ib = {iallglyphs[13],iallglyphs[7],iallglyphs[2],iallglyphs[6],iallglyphs[8],iallglyphs[9],iallglyphs[19],iallglyphs[22]}
+	-- Shaman - Restoration	
+		ic = {iallglyphs[7],iallglyphs[14],iallglyphs[3],iallglyphs[4],iallglyphs[5],iallglyphs[15],iallglyphs[20],iallglyphs[24],iallglyphs[29]}
+
+-- Warlock
+	jallglyphs = {
+		"Glyph of Conflagrate",
+		"Glyph of Curse of the Elements",
+		"Glyph of Curse of Exhaustion",
+		"Glyph of Dark Soul",
+		"Glyph of Demon Hunting",
+		"Glyph of Demon Training",
+		"Glyph of Demonic Circle",
+		"Glyph of Drain Life",
+		"Glyph of Ember Tap",
+		"Glyph of Eternal Resolve",
+		"Glyph of Everlasting Affliction",
+		"Glyph of Fear",
+		"Glyph of Healthstone",
+		"Glyph of Imp Swarm",
+		"Glyph of Life Tap",
+		"Glyph of Shadowflame",
+		"Glyph of Siphon Life",
+		"Glyph of Soul Consumption",
+		"Glyph of Soul Swap",
+		"Glyph of Soulstone",
+		"Glyph of Unstable Affliction",
+		"Glyph of Carrion Swarm",
+		"Glyph of Crimson Banish",
+		"Glyph of Enslave Demon",
+		"Glyph of Eye of Kilrogg",
+		"Glyph of Falling Meteor",
+		"Glyph of Gateway Attunement",
+		"Glyph of Felguard",
+		"Glyph of Hand of Gul'dan",
+		"Glyph of Health Funnel",
+		"Glyph of Nightmares",
+		"Glyph of Shadow Bolt",
+		"Glyph of Soulwell",
+		"Glyph of Subtlety",
+		"Glyph of Unending Breath",
+		"Glyph of Verdant Spheres",
+		"Glyph of Havoc",
+		"Glyph of Unending Resolve"};
+
+	-- Warlock - Affliction	
+		ja = {jallglyphs[17],jallglyphs[21],jallglyphs[2],jallglyphs[7],jallglyphs[10],jallglyphs[13],jallglyphs[20],jallglyphs[38],jallglyphs[23],jallglyphs[24]}
+	-- Warlock - Demonology
+		jb = {jallglyphs[17],jallglyphs[14],jallglyphs[2],jallglyphs[7],jallglyphs[5],jallglyphs[10],jallglyphs[13],jallglyphs[20],jallglyphs[38],jallglyphs[23],jallglyphs[24],jallglyphs[32]}
+	-- Warlock - Destruction
+		jc = {jallglyphs[17],jallglyphs[1],jallglyphs[2],jallglyphs[7],jallglyphs[10],jallglyphs[37],jallglyphs[13],jallglyphs[20],jallglyphs[38],jallglyphs[23],jallglyphs[24]}
+
+-- Warrior
+	kallglyphs = {
+		"Glyph of Blitz",
+		"Glyph of Bloodthirst",
+		"Glyph of Bull Rush",
+		"Glyph of Colossus Smash",
+		"Glyph of Death From Above",
+		"Glyph of Die by the Sword",
+		"Glyph of Enraged Speed",
+		"Glyph of Furious Sundering",
+		"Glyph of Gag Order",
+		"Glyph of Hamstring",
+		"Glyph of Heavy Repercussions",
+		"Glyph of Hindering Strikes",
+		"Glyph of Hoarse Voice",
+		"Glyph of Hold the Line",
+		"Glyph of Incite",
+		"Glyph of Long Charge",
+		"Glyph of Mortal Strike",
+		"Glyph of Raging Wind",
+		"Glyph of Rude Interruption",
+		"Glyph of Shield Slam",
+		"Glyph of Shield Wall",
+		"Glyph of Spell Reflection",	
+		"Glyph of Sweeping Strikes",
+		"Glyph of Unending Rage",
+		"Glyph of Victory Rush",
+		"Glyph of Whirlwind",
+		"Glyph of the Blazing Trail",
+		"Glyph of Bloodcurdling Shout",
+		"Glyph of Bloody Healing",
+		"Glyph of Burning Anger",
+		"Glyph of Crow Feast",
+		"Glyph of Gushing Wound",
+		"Glyph of Intimidating Shout",
+		"Glyph of Mighty Victory",
+		"Glyph of Mystic Shout",
+		"Glyph of Thunder Strike",
+		"Glyph of Subtle Defender",
+		"Glyph of Recklessness"};
+
+	-- Warrior - Arms		
+		ka = {kallglyphs[24],kallglyphs[5],kallglyphs[23],kallglyphs[3],kallglyphs[4],kallglyphs[12],kallglyphs[17],kallglyphs[19],kallglyphs[26],kallglyphs[33],kallglyphs[37]}
+	-- Warrior - Fury
+		kb = {kallglyphs[24],kallglyphs[5],kallglyphs[3],kallglyphs[4],kallglyphs[38],kallglyphs[18],kallglyphs[19],kallglyphs[26],kallglyphs[33],kallglyphs[37]}
+	-- Warrior - Protection
+		kc = {kallglyphs[24],kallglyphs[5],kallglyphs[3],kallglyphs[7],kallglyphs[11],kallglyphs[14],kallglyphs[15],kallglyphs[9],kallglyphs[33]}
